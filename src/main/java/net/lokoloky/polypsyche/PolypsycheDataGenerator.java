03d682561d506d8c3439c9eb408138743d5986b3
Lokoloky <lokoloky2009@gmail.com>
2025-07-20 22:30:51 +0200

    Initial commit

---
+++ src/main/java/net/lokoloky/polypsyche/PolypsycheDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lokoloky.polypsyche;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PolypsycheDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
