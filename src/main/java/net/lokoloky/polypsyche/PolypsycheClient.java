0f2913529d172ec9ea7c855e17ed2f6c19789d2f
Lokoloky <lokoloky2009@gmail.com>
2025-07-20 22:41:09 +0200

    Base changes

---
+++ src/main/java/net/lokoloky/polypsyche/PolypsycheClient.java
@@ -0,0 +1,10 @@
+package net.lokoloky.polypsyche;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PolypsycheClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
