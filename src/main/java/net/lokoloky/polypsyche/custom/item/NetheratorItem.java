43d16bd4e91b38f2ce9eb4b41c15938d1771a0ca
Lokoloky <lokoloky2009@gmail.com>
2025-08-26 03:04:18 +0200

    hola

---
+++ src/main/java/net/lokoloky/polypsyche/custom/item/NetheratorItem.java
@@ -0,0 +1,51 @@
+package net.lokoloky.polypsyche.custom.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SaplingBlock;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
+
+import java.util.Map;
+
+public class NetheratorItem extends Item {
+    private static final Map<Block, Block> NETHERATOR_MAP =
+            Map.of(
+                    Blocks.STONE, Blocks.BLACKSTONE,
+                    Blocks.DIRT, Blocks.NETHERRACK,
+                    Blocks.GRASS_BLOCK, Blocks.WARPED_NYLIUM,
+                    Blocks.OAK_SAPLING, Blocks.WARPED_FUNGUS,
+                    Blocks.OAK_PLANKS, Blocks.WARPED_PLANKS,
+                    Blocks.OAK_SLAB, Blocks.WARPED_SLAB,
+                    Blocks.OAK_STAIRS, Blocks.WARPED_STAIRS,
+                    Blocks.OAK_LOG, Blocks.WARPED_STEM
+            );
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        Block clickedBlock = world.getBlockState(context.getBlockPos()).getBlock();
+        if (NETHERATOR_MAP.containsKey(clickedBlock)) {
+            if (!world.isClient) {
+                world.setBlockState(context.getBlockPos(), NETHERATOR_MAP.get(clickedBlock).getDefaultState());
+
+                world.playSound(null, context.getBlockPos(), SoundEvents.BLOCK_NETHER_WOOD_PLACE, SoundCategory.BLOCKS);
+            }
+        }
+
+
+
+        return ActionResult.SUCCESS;
+    };
+
+    public NetheratorItem(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/net/lokoloky/polypsyche/item/ModItems.java
+++ src/main/java/net/lokoloky/polypsyche/item/ModItems.java
@@ -2,6 +2,8 @@ package net.lokoloky.polypsyche.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lokoloky.polypsyche.Polypsyche;
+import net.lokoloky.polypsyche.custom.item.ChiselItem;
+import net.lokoloky.polypsyche.custom.item.NetheratorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -11,6 +13,9 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(64)));
+    public static final Item NETHERATOR = registerItem("netherator", new NetheratorItem(new Item.Settings().maxDamage(1)));
 
 
     private static Item registerItem(String name, Item item){
